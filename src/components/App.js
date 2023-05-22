1dcaf4b2105d42cf1cad920ff0c97735150c78f5
Vitaly Kryanin <92264488+VKryanin@users.noreply.github.com>
2023-05-22 17:25:34 +0300

    popup

--- src/components/App.js
+++ src/components/App.js
@@ -17,9 +17,6 @@ function App() {
     localStorage.clear()
     setLoggedIn(false)
   }
-
-
-
   return (
     <>
       <Routes>
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import * as mestoAuth from '../utils/MestoAuth'
+import { Message } from "./Message";
 
 export function Signup() {
   const [formValue, setFormValue] = useState({
@@ -9,7 +10,8 @@ export function Signup() {
   })
   const [errorMessage, setErrorMessage] = useState('');
   const navigate = useNavigate();
-
+  const [status, setStatus] = useState('')
+  const [style, setStyle] = useState({display: 'none'})
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormValue({
@@ -24,20 +26,30 @@ export function Signup() {
     mestoAuth.register(email, password)
       .then(() => {
         navigate('/sign-in');
+        setStatus(true);
+        setStyle({
+          display: 'block'
+        })
       })
-      .catch(err => setErrorMessage(err));
+      .catch(err => {
+        setStatus(false)
+        setErrorMessage(err)
+        setStyle({
+          display: 'block'
+        })
+        console.log(err);
+      });
   }
 
   return (
     <div className="authForm__container">
+      <Message status={status} styles={style}/>
       <h2 className="authForm__title">Регистрация</h2>
       <form onSubmit={handleSubmit} className="authForm__form">
-
         <input id="email" name="email" type="email" autoComplete="email" value={formValue.email}
-          onChange={handleChange} placeholder="Email"/>
+          onChange={handleChange} placeholder="Email" />
         <input id="password" name="password" type="password" autoComplete="new-password" value={formValue.password}
-          onChange={handleChange} placeholder="Пароль"/>
-
+          onChange={handleChange} placeholder="Пароль" />
         <div className="authForm__button-container">
           <button type="submit" className="authForm__link">Зарегистрироваться</button>
         </div>
